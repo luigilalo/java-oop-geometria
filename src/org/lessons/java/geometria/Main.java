9eac9c6197665738cf4aef8ee9316722cd9cb5e3
luigilalo <luigilaloe1@gmail.com>
2023-12-05 15:12:32 +0100

    Exercise Done without Bonus

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -1,2 +1,28 @@
-package org.lessons.java.geometria;public class Main {
+package org.lessons.java.geometria;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Rectangle rectangle = new Rectangle(10, 20);
+
+        // METTO A SCHERMO BASE E ALTEZZA
+        System.out.println("La base del rettangolo misura " + rectangle.base);
+        System.out.println("L'altezza del rettangolo misura " + rectangle.altezza);
+        System.out.println("L'area del rettangolo misura " + rectangle.area);
+        System.out.println("Il perimetro del rettangolo misura " + rectangle.perimetro);
+
+        System.out.println("Vuoi creare un nuovo rettangolo? (y/n)");
+        String rectangleChoice = scan.next();
+        if (rectangleChoice.equalsIgnoreCase("y")) {
+            System.out.println("Quanto deve misurare la base?");
+            int base = scan.nextInt();
+            System.out.println("Quanto deve misurare l'altezza?");
+            int altezza = scan.nextInt();
+            Rectangle rectangleCreated = new Rectangle(base, altezza);
+            System.out.println ("L'area del nuovo rettangolo sarà " + rectangleCreated.area);
+            System.out.println ("Il perimetro del nuovo rettangolo sarà " + rectangleCreated.perimetro);
+        }
+    }
 }
--- src/org/lessons/java/geometria/Rectangle.java
+++ src/org/lessons/java/geometria/Rectangle.java
@@ -1,2 +1,19 @@
-package org.lessons.java.geometria;public class Rectangle {
+package org.lessons.java.geometria;
+
+public class Rectangle {
+    // AGGIUNGO GLI ATTRIBUTI DEL RETTANGOLO
+    int base;
+    int altezza;
+    int area;
+    int perimetro;
+
+    // METODO
+    Rectangle(int base, int altezza) {
+        this.base = base;
+        this.altezza = altezza;
+        this.area = base * altezza;
+        this.perimetro = 2*base + 2*altezza;
+    }
+
+
 }
